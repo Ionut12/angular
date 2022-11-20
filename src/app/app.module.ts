9a050bd891fd3e1de926f96b67f7848791ee55d5
Ionut12 <plesaionut1994@yahoo.ro>
2022-11-20 15:50:20 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { SelectorComponent } from './selector/selector.component';
+import { AddPersonComponent } from './add-person/add-person.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, SelectorComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SelectorComponent,
+    AddPersonComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
